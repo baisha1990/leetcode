bb758f1cafa90bdd32a3eed6b89b9adf984e8574
baisha1990 <baisha1990@gmail.com>
2020-04-30 16:16:47 -0700

    Contiguous array

---
+++ 30-Day Leetcoding Challenge April/Contiguous array/Solution.java
@@ -0,0 +1,32 @@
+/*Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+
+Example 1:
+Input: [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+Example 2:
+Input: [0,1,0]
+Output: 2
+Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+Note: The length of the given binary array will not exceed 50,000.*/
+
+
+class Solution {
+    public int findMaxLength(int[] nums) {
+        int l = 0, zero = 0, one = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) 
+                zero++;
+            else 
+                one++;
+            
+            if(map.containsKey(zero-one))
+                l = Math.max(l, i-map.get(zero-one));
+            else
+                map.put(zero-one, i);
+        }
+        return l;
+    }
+}
