09c015c82b353bc98cf25d5c29715a32da9d21de
baisha1990 <baisha1990@gmail.com>
2020-04-30 18:30:38 -0700

    Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree

---
+++ 30-Day Leetcoding Challenge April/Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree/Solution.java
@@ -0,0 +1,64 @@
+/*Given a binary tree where each path going from the root to any leaf form a valid sequence, check if a given string is a valid sequence in such binary tree. 
+
+We get the given string from the concatenation of an array of integers arr and the concatenation of all values of the nodes along a path results in a sequence in the given binary tree.
+
+Example 1:
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,0,1]
+Output: true
+Explanation: 
+The path 0 -> 1 -> 0 -> 1 is a valid sequence (green color in the figure). 
+Other valid sequences are: 
+0 -> 1 -> 1 -> 0 
+0 -> 0 -> 0
+
+Example 2:
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,0,1]
+Output: false 
+Explanation: The path 0 -> 0 -> 1 does not exist, therefore it is not even a sequence.
+
+Example 3:
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,1]
+Output: false
+Explanation: The path 0 -> 1 -> 1 is a sequence, but it is not a valid sequence.
+ 
+
+Constraints:
+
+1 <= arr.length <= 5000
+0 <= arr[i] <= 9
+Each node's value is between [0 - 9].*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidSequence(TreeNode root, int[] arr) {
+        if(root == null) return arr.length == 0;
+        return isValidSequence(root, arr, 0);
+    }
+    
+    public boolean isValidSequence(TreeNode root, int[] arr, int index) {
+        if(root.val != arr[index])
+            return false;
+        
+        if(index == arr.length-1)
+            return root.left == null && root.right == null;
+        
+        return ((root.left != null && isValidSequence(root.left, arr, index+1)) || (root.right != null && isValidSequence(root.right, arr, index+1))); 
+    }
+}
