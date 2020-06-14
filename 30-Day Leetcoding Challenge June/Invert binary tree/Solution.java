6fe0a40ce4b09b7dac86105faaeb92f953128a17
baisha1990 <baisha1990@gmail.com>
2020-06-14 14:25:05 -0700

    Invert binary tree

---
+++ 30-Day Leetcoding Challenge June/Invert binary tree/Solution.java
@@ -0,0 +1,52 @@
+/*Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        helper(root);
+        return root;
+    }
+    
+    void helper(TreeNode root) {
+        if(root == null)
+            return;
+        
+        TreeNode t = root.left;
+        root.left = root.right;
+        root.right = t;
+        
+        helper(root.left);
+        helper(root.right);
+    }
+}
