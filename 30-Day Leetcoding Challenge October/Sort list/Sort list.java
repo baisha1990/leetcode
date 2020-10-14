abaf8daa2453518fece3270530d0e354c0193d2d
baisha1990 <baisha1990@gmail.com>
2020-10-13 22:07:12 -0700

    Sort list

---
+++ 30-Day Leetcoding Challenge October/Sort list/Sort list.java
@@ -0,0 +1,82 @@
+/*Given the head of a linked list, return the list after sorting it in ascending order.
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+
+ 
+
+Example 1:
+
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105*
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode temp = head;
+        while(fast != null && fast.next != null) {
+            temp = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        temp.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+        
+        return merge(left, right);
+    }
+    
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode newList = new ListNode(0);
+        ListNode temp = newList;
+        
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        if(l1 != null) {
+            temp.next = l1;
+            l1 = l1.next;
+        }
+        if(l2 != null) {
+            temp.next = l2;
+            l2 = l2.next;
+        }
+        return newList.next;
+    }
+}
