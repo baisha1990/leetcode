a2545ea192fd7616bdbe31b2025280fd52aaf898
baisha1990 <baisha1990@gmail.com>
2020-10-31 14:29:15 -0700

    Recover binary search tree

---
+++ 30-Day Leetcoding Challenge October/Recover binary search tree/Recover binary search tree.java
@@ -0,0 +1,64 @@
+/*You are given the root of a binary search tree (BST), where exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
+
+Follow up: A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+
+Example 1:
+Input: root = [1,3,null,null,2]
+Output: [3,1,null,null,2]
+Explanation: 3 cannot be a left child of 1 because 3 > 1. Swapping 1 and 3 makes the BST valid.
+
+Example 2:
+Input: root = [3,1,4,null,null,2]
+Output: [2,1,4,null,null,3]
+Explanation: 2 cannot be in the right subtree of 3 because 2 < 3. Swapping 2 and 3 makes the BST valid.
+
+Constraints:
+
+The number of nodes in the tree is in the range [2, 1000].
+-231 <= Node.val <= 231 - 1*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode f, s, prev;
+    public void recoverTree(TreeNode root) {
+        if(root == null)
+            return;
+        inorder(root);
+        if(s != null && f != null) {
+            int val = s.val;
+            s.val = f.val;
+            f.val = val;
+        }
+    }
+    
+    public void inorder(TreeNode root) {
+        if(root == null)
+            return;
+        inorder(root.left);
+        if(prev == null)
+            prev = root;
+        else{
+            if(root.val < prev.val) {
+                if(f == null)
+                    f = prev;
+                s = root;
+            }
+            prev = root;
+        }
+        inorder(root.right);
+    }
+}
