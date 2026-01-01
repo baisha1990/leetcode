72436e46145adec519bbd1a9531eab04c4f188df
baisha1990 <baisha1990@gmail.com>
2025-12-31 21:40:15 -0800

    Valid Boomerang

---
+++ Misc/Valid Boomerang/Solution.java
@@ -0,0 +1,24 @@
+/*
+Given an array points where points[i] = [xi, yi] represents a point on the X-Y plane, return true if these points are a boomerang.
+A boomerang is a set of three points that are all distinct and not in a straight line.
+
+Example 1:
+Input: points = [[1,1],[2,3],[3,2]]
+Output: true
+
+Example 2:
+Input: points = [[1,1],[2,2],[3,3]]
+Output: false
+
+Constraints:
+points.length == 3
+points[i].length == 2
+0 <= xi, yi <= 100
+*/
+
+class Solution {
+    public boolean isBoomerang(int[][] points) {
+        int x1 = points[0][0], x2 = points[1][0], x3 = points[2][0], y1 = points[0][1], y2 = points[1][1], y3 = points[2][1];
+        return (((y2 - y1) * (x3 - x2)) != ((y3 - y2) * (x2 - x1)));
+    }
+}
