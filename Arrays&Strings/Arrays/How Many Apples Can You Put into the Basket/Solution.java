cf61e9acb22f0a082dc3a5bde8b08b4ecca097ad
baisha1990 <baisha1990@gmail.com>
2026-01-18 16:14:53 -0800

    How Many Apples Can You Put into the Basket

---
+++ Arrays&Strings/Arrays/How Many Apples Can You Put into the Basket/Solution.java
@@ -0,0 +1,30 @@
+/*
+You have some apples and a basket that can carry up to 5000 units of weight.
+Given an integer array weight where weight[i] is the weight of the ith apple, return the maximum number of apples you can put in the basket.
+
+Example 1:
+Input: weight = [100,200,150,1000]
+Output: 4
+Explanation: All 4 apples can be carried by the basket since their sum of weights is 1450.
+
+Example 2:
+Input: weight = [900,950,800,1000,700,800]
+Output: 5
+Explanation: The sum of weights of the 6 apples exceeds 5000 so we choose any 5 of them.
+
+Constraints:
+1 <= weight.length <= 103
+1 <= weight[i] <= 103
+*/
+
+class Solution {
+    public int maxNumberOfApples(int[] weight) {
+        Arrays.sort(weight);
+        int weightSoFar = 0, numberOfApples = 0;
+        for(int i = 0; i < weight.length && weightSoFar + weight[i] <= 5000; i++) {
+            weightSoFar += weight[i];
+            numberOfApples++;
+        }
+        return numberOfApples;
+    }
+}
