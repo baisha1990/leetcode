931772d230584f1d5622eb87e225a3f5c58ac181
baisha1990 <baisha1990@gmail.com>
2026-01-23 21:46:02 -0800

    Sort Integers by The Number of 1 Bits

---
+++ Hashtable/Sort Integers by The Number of 1 Bits/Solution.java
@@ -0,0 +1,60 @@
+/*
+You are given an integer array arr. Sort the integers in the array in ascending order by the number of 1's in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
+
+Return the array after sorting it.
+
+ 
+
+Example 1:
+
+Input: arr = [0,1,2,3,4,5,6,7,8]
+Output: [0,1,2,4,8,3,5,6,7]
+Explantion: [0] is the only integer with 0 bits.
+[1,2,4,8] all have 1 bit.
+[3,5,6] have 2 bits.
+[7] has 3 bits.
+The sorted array by bits is [0,1,2,4,8,3,5,6,7]
+Example 2:
+
+Input: arr = [1024,512,256,128,64,32,16,8,4,2,1]
+Output: [1,2,4,8,16,32,64,128,256,512,1024]
+Explantion: All integers have 1 bit in the binary representation, you should just sort them in ascending order.
+ 
+
+Constraints:
+
+1 <= arr.length <= 500
+0 <= arr[i] <= 104
+*/
+
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
+        for(int i = 0; i < arr.length; i++) {
+            int count = countOnes(arr[i]);
+            map.putIfAbsent(count, new ArrayList<>());
+            List<Integer> l = map.get(count);
+            l.add(arr[i]);
+            map.put(count, l);
+        }
+        int[] result = new int[arr.length];
+        int k = 0;
+        for(Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
+            List<Integer> l = entry.getValue();
+            Collections.sort(l);
+            for(int i = 0; i < l.size(); i++) {
+                result[k] = l.get(i);
+                k++;
+            }
+        }
+        return result;
+    }
+    private int countOnes(int a) {
+        int count = 0;
+        while(a != 0) {
+            a &= (a - 1);
+            count++;
+        }
+        return count;
+    }
+}
