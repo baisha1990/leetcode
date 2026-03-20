9f3c8003658d152a032704cee596cf2fcfc05ee0
baisha1990 <baisha1990@gmail.com>
2026-03-19 19:29:47 -0700

    The k-th Lexicographical String of All Happy Strings of Length n

---
+++ 30-Day Leetcoding Challenge March/The k-th Lexicographical String of All Happy Strings of Length n/Solution.java
@@ -0,0 +1,58 @@
+/*
+A happy string is a string that:
+consists only of letters of the set ['a', 'b', 'c'].
+s[i] != s[i + 1] for all values of i from 1 to s.length - 1 (string is 1-indexed).
+For example, strings "abc", "ac", "b" and "abcbabcbcb" are all happy strings and strings "aa", "baa" and "ababbc" are not happy strings.
+Given two integers n and k, consider a list of all happy strings of length n sorted in lexicographical order.
+Return the kth string of this list or return an empty string if there are less than k happy strings of length n.
+
+Example 1:
+Input: n = 1, k = 3
+Output: "c"
+Explanation: The list ["a", "b", "c"] contains all happy strings of length 1. The third string is "c".
+
+Example 2:
+Input: n = 1, k = 4
+Output: ""
+Explanation: There are only 3 happy strings of length 1.
+
+Example 3:
+Input: n = 3, k = 9
+Output: "cab"
+Explanation: There are 12 different happy string of length 3 ["aba", "abc", "aca", "acb", "bab", "bac", "bca", "bcb", "cab", "cac", "cba", "cbc"]. You will find the 9th string = "cab"
+ 
+Constraints:
+1 <= n <= 10
+1 <= k <= 100
+*/
+
+class Solution {
+
+    public String getHappyString(int n, int k) {
+        Stack<String> stringsStack = new Stack<>();
+        int indexInSortedList = 0;
+        stringsStack.push(""); 
+
+        while (stringsStack.size() > 0) {
+            String currentString = stringsStack.pop();
+            if (currentString.length() == n) {
+                indexInSortedList++;
+                if (indexInSortedList == k) {
+                    return currentString;
+                }
+                continue;
+            }
+
+            for (char currentChar = 'c'; currentChar >= 'a'; currentChar--) {
+                if (
+                    currentString.length() == 0 ||
+                    currentString.charAt(currentString.length() - 1) !=
+                    currentChar
+                ) {
+                    stringsStack.push(currentString + currentChar);
+                }
+            }
+        }
+        return "";
+    }
+}
