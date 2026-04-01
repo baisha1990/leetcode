07f1980eca623a35514bc843ed6324c11c05e4cb
baisha1990 <baisha1990@gmail.com>
2026-03-31 19:09:37 -0700

    Equal Sum Grid Partition I

---
+++ 30-Day Leetcoding Challenge March/Equal Sum Grid Partition I/Solution.java
@@ -0,0 +1,64 @@
+/*
+You are given an m x n matrix grid of positive integers. Your task is to determine if it is possible to make either one horizontal or one vertical cut on the grid such that:
+Each of the two resulting sections formed by the cut is non-empty.
+The sum of the elements in both sections is equal.
+Return true if such a partition exists; otherwise return false.
+
+Example 1:
+Input: grid = [[1,4],[2,3]]
+Output: true
+Explanation:
+A horizontal cut between row 0 and row 1 results in two non-empty sections, each with a sum of 5. Thus, the answer is true.
+
+Example 2:
+Input: grid = [[1,3],[2,4]]
+Output: false
+Explanation:
+No horizontal or vertical cut results in two non-empty sections with equal sums. Thus, the answer is false.
+
+Constraints:
+1 <= m == grid.length <= 105
+1 <= n == grid[i].length <= 105
+2 <= m * n <= 105
+1 <= grid[i][j] <= 105
+*/
+
+class Solution {
+    public boolean canPartitionGrid(int[][] grid) {
+        long total = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                total += grid[i][j];
+            }
+        }
+        for (int k = 0; k < 2; k++) {
+            long sum = 0;
+            m = grid.length;
+            n = grid[0].length;
+            for (int i = 0; i < m - 1; i++) {
+                for (int j = 0; j < n; j++) {
+                    sum += grid[i][j];
+                }
+                if (sum * 2 == total) {
+                    return true;
+                }
+            }
+            grid = rotation(grid);
+        }
+        return false;
+    }
+
+    public int[][] rotation(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] tmp = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                tmp[j][m - 1 - i] = grid[i][j];
+            }
+        }
+        return tmp;
+    }
+}
