fd82bc604605d3890741b5863961789ac730115f
baisha1990 <baisha1990@gmail.com>
2026-05-27 07:03:54 -0700

    Most Frequent Even Element

---
+++ Arrays&Strings/Arrays/Most Frequent Even Element/Solution.java
@@ -0,0 +1,45 @@
+/*
+Given an integer array nums, return the most frequent even element.
+If there is a tie, return the smallest one. If there is no such element, return -1.
+
+Example 1:
+Input: nums = [0,1,2,2,4,4,1]
+Output: 2
+Explanation:
+The even elements are 0, 2, and 4. Of these, 2 and 4 appear the most.
+We return the smallest one, which is 2.
+
+Example 2:
+Input: nums = [4,4,4,9,2,4]
+Output: 4
+Explanation: 4 is the even element appears the most.
+
+Example 3:
+Input: nums = [29,47,21,41,13,37,25,7]
+Output: -1
+Explanation: There is no even element.
+ 
+Constraints:
+1 <= nums.length <= 2000
+0 <= nums[i] <= 105
+*/
+
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        int n = nums.length;
+        int freq[] = new int[100001];
+        int ans = -1, maxFreq = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 == 0) {
+                freq[nums[i]]++;
+                if (freq[nums[i]] == maxFreq) {
+                    ans = Math.min(ans, nums[i]);
+                } else if (freq[nums[i]] > maxFreq) {
+                    ans = nums[i];
+                    maxFreq = freq[nums[i]];
+                }
+            }
+        }
+        return ans;
+    }
+}
