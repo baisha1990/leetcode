41f007d9ecc3dbfa64573794112751de45a5cf65
baisha1990 <baisha1990@gmail.com>
2026-06-02 06:47:40 -0700

    Remove Letter To Equalize Frequency

---
+++ HashSet/Remove Letter To Equalize Frequency/Solution.java
@@ -0,0 +1,52 @@
+/*
+You are given a 0-indexed string word, consisting of lowercase English letters. You need to select one index and remove the letter at that index from word so that the frequency of every letter present in word is equal.
+Return true if it is possible to remove one letter so that the frequency of all letters in word are equal, and false otherwise.
+Note:
+The frequency of a letter x is the number of times it occurs in the string.
+You must remove exactly one letter and cannot choose to do nothing.
+
+Example 1:
+Input: word = "abcc"
+Output: true
+Explanation: Select index 3 and delete it: word becomes "abc" and each character has a frequency of 1.
+
+Example 2:
+Input: word = "aazz"
+Output: false
+Explanation: We must delete a character, so either the frequency of "a" is 1 and the frequency of "z" is 2, or vice versa. It is impossible to make all present letters have equal frequency.
+ 
+Constraints:
+2 <= word.length <= 100
+word consists of lowercase English letters only.
+*/
+
+class Solution {
+    public boolean equalFrequency(String word) {
+        int[] c = new int[26];
+        int n = word.length();
+        for (int i = 0; i < n; i++) {
+            c[word.charAt(i)-'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if(c[i] == 0) {
+                continue;
+            }
+            c[i]--;
+            if(isEqual(c)) {
+                return true;
+            }
+            c[i]++;
+        }
+        return false;
+    }
+	
+    private boolean isEqual(int[] c) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < 26; i++) {
+            if (c[i] != 0) {
+                set.add(c[i]);
+            }
+        }
+        return set.size() < 2;
+    }
+}
