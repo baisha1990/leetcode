aa35c632dc5e2548d8e4f0394a5556a5c052fa7a
baisha1990 <baisha1990@gmail.com>
2026-08-19 18:31:19 -0700

    Clear Digits

---
+++ Stack/Clear Digits/Solution.java
@@ -0,0 +1,48 @@
+/*
+You are given a string s.
+Your task is to remove all digits by doing this operation repeatedly:
+Delete the first digit and the closest non-digit character to its left.
+Return the resulting string after removing all digits.
+Note that the operation cannot be performed on a digit that does not have any non-digit character to its left.
+
+Example 1:
+Input: s = "abc"
+Output: "abc"
+Explanation:
+There is no digit in the string.
+
+Example 2:
+Input: s = "cb34"
+Output: ""
+Explanation:
+First, we apply the operation on s[2], and s becomes "c4".
+Then we apply the operation on s[1], and s becomes "".
+
+Constraints:
+1 <= s.length <= 100
+s consists only of lowercase English letters and digits.
+The input is generated such that it is possible to delete all digits.
+*/
+
+class Solution {
+    public String clearDigits(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(stack.isEmpty()) {
+                stack.push(c);
+                continue;
+            }
+            if(Character.isDigit(c) && !Character.isDigit(stack.peek())) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        while(!stack.isEmpty()) {
+            res.append(stack.pop());
+        }
+        return res.reverse().toString();
+    }
+}
